8a3ae6daee46babe64531a7c05bc61bb24a9f233
patrice <patrice@dudule.home>
2018-11-15 22:19:06 +0100

    Ajout de la bibliothèque de gestion des encodeurs

---
+++ ITER_encoders/.ino.cpp
@@ -0,0 +1,16 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2018-11-14 20:39:09
+
+#include "Arduino.h"
+#include "Arduino.h"
+#include "IterEncoders.h"
+void setup() ;
+void loop() ;
+
+#include "ITER_encoders.ino"
+
+
+#endif
---
+++ ITER_encoders/Release/.ino.cpp.d
@@ -0,0 +1,39 @@
+.ino.cpp.o: ..\.ino.cpp \
+ C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/Arduino.h \
+ C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/binary.h \
+ C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/WCharacter.h \
+ C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/WString.h \
+ C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/HardwareSerial.h \
+ C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/Stream.h \
+ C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/Print.h \
+ C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/Printable.h \
+ C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/USBAPI.h \
+ C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/Arduino.h \
+ C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\variants\mega/pins_arduino.h \
+ ..\IterEncoders.h ..\ITER_encoders.ino
+
+C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/Arduino.h:
+
+C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/binary.h:
+
+C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/WCharacter.h:
+
+C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/WString.h:
+
+C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/HardwareSerial.h:
+
+C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/Stream.h:
+
+C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/Print.h:
+
+C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/Printable.h:
+
+C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/USBAPI.h:
+
+C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino/Arduino.h:
+
+C:\EclipseNeon3\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\variants\mega/pins_arduino.h:
+
+..\IterEncoders.h:
+
+..\ITER_encoders.ino:
