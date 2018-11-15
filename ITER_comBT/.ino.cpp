0e497ccb4ef2c16a13ab70829ef7c33e8dafcbe4
patrice <patrice@dudule.home>
2018-11-15 22:14:28 +0100

    commit pat

--- ITER_comBT/.ino.cpp
+++ ITER_comBT/.ino.cpp
@@ -2,19 +2,14 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-11-15 22:10:48
+//This file has been generated on 2018-11-15 22:13:50
 
 #include "Arduino.h"
 #include "Arduino.h"
-#include "Arduino.h"
-#include "IterEncoders.h"
-void setup() ;
-void loop() ;
 void setup() ;
 void loop() ;
 
 #include "ITER_comBT.ino"
 
-#include "ITER_encoders.ino"
 
 #endif
